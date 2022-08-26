9ec13a5ac5ece33aac3534fe392ed0a24fc47d4a
tamirdavidi1 <tamirdavidi1@mail.tau.ac.il>
2022-08-27 00:43:38 +0300

    a

--- index.js
+++ index.js
@@ -6,7 +6,12 @@
 
 
 
-
+let username;
+document.getElementById("myButton").onclick = function () {
+    username = document.getElementById("myText").value;
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+    console.log(username);
+}
 
 
 
