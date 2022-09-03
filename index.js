e3b0abb77e4d0868b778cd56e1a8f8c8de77c2b4
tamirdavidi1 <tamirdavidi1@mail.tau.ac.il>
2022-09-03 13:55:32 +0300

    '#'

--- index.js
+++ index.js
@@ -13,7 +13,16 @@ document.getElementById("myButton").onclick = function () {
     console.log(username);
 }
 
-
+let a
+let b
+let c  
+document.getElementById("submit").onclick = function () {
+    a = Number(document.getElementById("a").value);
+    b = Number(document.getElementById("b").value);
+    c = Math.sqrt(a*a + b*b);
+    document.getElementById("c").innerHTML = "YETER RESULT: " + c;
+    console.log(c);
+}
 
 
 
